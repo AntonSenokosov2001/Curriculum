41062d04ccb16214ee0c2006169da70b184c3ddf
AntonSenokosov2001 <a.y.senokosov@student.khai.edu>
2021-11-11 04:41:01 +0200

    Create forms Autorization and MainForm

---
+++ Curriculum/Curriculum.WinUI/Authorization.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Curriculum.WinUI
+{
+    public partial class Authorization : Form
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (login == "mylogin" && password == "qwerty12345")
+            {
+                MainForm mainForm = new MainForm();
+                mainForm.Show();
+                this.Hide();
+            }
+            else if ((login == "mylogin" && password != "qwerty12345") || (login != "mylogin" && password == "qwerty12345") || (login != "mylogin" && password != "qwerty12345"))
+            {
+                MessageBox.Show("Неправильний логін або пароль");
+            }
+        }
+    }
+}
