96cf91193035a4fda90ccc3e57a1fe8677696c9f
AntonSenokosov2001 <a.y.senokosov@student.khai.edu>
2021-11-11 05:19:33 +0200

    Create folder Abstract and add interfaces to classes

---
+++ Curriculum/Curriculum.Domain/Abstract/IBachelorFourYear.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Curriculum.Domain.Entities;
+
+namespace Curriculum.Domain.Abstract
+{
+    public class IBachelorFourYear
+    {
+        public IEnumerable<BachelorFourYear> BachelorFourYears { get; }
+    }
+}
